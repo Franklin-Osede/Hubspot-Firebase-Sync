a12e3f4aa9eeb1f286fb0bfe7d27408df97274a7
Franklin <71532004+fran1014@users.noreply.github.com>
2025-01-25 15:03:14 +0100

    updated config ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
-// config.ts
 export const config = {
   hubspot: {
     apiKey: process.env.HUBSPOT_API_KEY
   },
   firebase: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
     useEmulator: process.env.USE_FIREBASE_EMULATOR === 'true'
   }
 };
